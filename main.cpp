4860b0783b25664e3a75799a346408e31ae95027
Sainan <sainan@calamity.gg>
2024-05-12 15:09:53 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,190 @@
+#define LOGGING false
+
+#if LOGGING
+#include <iostream>
+#endif
+
+#include <DetourHook.hpp>
+#include <Module.hpp>
+#include <Pattern.hpp>
+#include <pattern_macros.hpp>
+
+using namespace soup;
+
+static HMODULE og_lib;
+static FARPROC og_DwmGetCompositionTimingInfo;
+
+extern "C" __declspec(dllexport) void DwmGetCompositionTimingInfo() { og_DwmGetCompositionTimingInfo(); }
+
+struct ParsedUrl
+{
+	char pad[16];
+	char host[256];
+};
+
+static DetourHook parse_url_hook;
+
+static bool parse_url_detour(const char* in, ParsedUrl* out)
+{
+#if LOGGING
+	std::cout << "parse_url " << in << std::endl;
+#endif
+	in = "https://localhost/origin/CAFEBABE"; // SpaceNinjaServer expects this kind of path prefix
+	if (reinterpret_cast<decltype(&parse_url_detour)>(parse_url_hook.original)(in, out))
+	{
+		//strcpy(out->host, "localhost");
+		return true;
+	}
+	return false;
+}
+
+static DetourHook Curl_resolv_hook;
+
+static void* Curl_resolv_detour(void* a1, const char* hostname, int port, bool allowDOH, void* a5)
+{
+#if LOGGING
+	std::cout << "Curl_resolv for " << hostname << ", port " << port << std::endl;
+#endif
+	return reinterpret_cast<decltype(&Curl_resolv_detour)>(Curl_resolv_hook.original)(a1, "localhost", port, allowDOH, a5);
+}
+
+static DetourHook ssl_verify_internal_hook;
+
+static int64_t ssl_verify_internal_detour(void* a1, void* a2)
+{
+	//std::cout << "ssl_verify_internal called" << std::endl;
+	return 1; // "Verify success"
+}
+
+static DetourHook Curl_ossl_verifyhost_hook;
+
+static int64_t Curl_ossl_verifyhost_detour(void* a1, void* a2)
+{
+	//auto ret = reinterpret_cast<decltype(&Curl_ossl_verifyhost_detour)>(Curl_ossl_verifyhost_hook.original)(a1, a2);
+	//std::cout << "Curl_ossl_verifyhost returned " << ret << std::endl;
+	return 0;
+}
+
+static DetourHook verify_worldstate_integrity_hook;
+
+static bool verify_worldstate_integrity_detour()
+{
+	return true;
+}
+
+/*static DetourHook int_rsa_verify_hook;
+
+static int64_t int_rsa_verify_detour(void* a1, void* a2, void* a3, void* a4, size_t* a5, void* a6, void* a7, void* a8)
+{
+	//auto ret = reinterpret_cast<decltype(&int_rsa_verify_detour)>(int_rsa_verify_hook.original)(a1, a2, a3, a4, a5, a6, a7, a8);
+	//std::cout << "int_rsa_verify returns " << ret << std::endl;
+	if (a5)
+	{
+		*a5 = 1; // For > 0 return from pkey_rsa_verifyrecover
+	}
+	return 1;
+}*/
+
+BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hmod);
+
+#if LOGGING
+		AllocConsole();
+		{
+			FILE* f;
+			freopen_s(&f, "CONIN$", "r", stdin);
+			freopen_s(&f, "CONOUT$", "w", stderr);
+			freopen_s(&f, "CONOUT$", "w", stdout);
+		}
+#endif
+
+		{
+			std::wstring path(_wgetenv(L"windir"));
+			path.append(LR"(\System32\dwmapi.dll)");
+			og_lib = LoadLibraryW(path.c_str());
+#if LOGGING
+			std::cout << "og_lib = " << (void*)og_lib << std::endl;
+#endif
+			og_DwmGetCompositionTimingInfo = GetProcAddress(og_lib, "DwmGetCompositionTimingInfo");
+		}
+
+		{
+			SIG_INST("48 89 5C 24 18 55 56 57 48 8D AC 24 30 F6 FF FF 48 81 EC D0 0A 00 00");
+			auto parse_url = Module(nullptr).range.scan(sig_inst).as<void*>();
+#if LOGGING
+			std::cout << "parse_url = " << parse_url << std::endl;
+#endif
+			parse_url_hook.detour = reinterpret_cast<void*>(&parse_url_detour);
+			parse_url_hook.target = parse_url;
+			parse_url_hook.create();
+			parse_url_hook.enable();
+		}
+
+		{
+			SIG_INST("48 89 5C 24 20 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 50 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 40 48 8B 39");
+			auto Curl_resolv = Module(nullptr).range.scan(Pattern(sig_inst)).as<void*>();
+#if LOGGING
+			std::cout << "Curl_resolv = " << Curl_resolv << std::endl;
+#endif
+			Curl_resolv_hook.detour = reinterpret_cast<void*>(&Curl_resolv_detour);
+			Curl_resolv_hook.target = Curl_resolv;
+			Curl_resolv_hook.create();
+			Curl_resolv_hook.enable();
+		}
+
+		{
+			SIG_INST("48 89 5C 24 18 48 89 6C 24 20 56 48 83 EC 30 33 ED");
+			auto ssl_verify_internal = Module(nullptr).range.scan(Pattern(sig_inst)).as<void*>();
+#if LOGGING
+			std::cout << "ssl_verify_internal = " << ssl_verify_internal << std::endl;
+#endif
+			ssl_verify_internal_hook.detour = reinterpret_cast<void*>(&ssl_verify_internal_detour);
+			ssl_verify_internal_hook.target = ssl_verify_internal;
+			ssl_verify_internal_hook.create();
+			ssl_verify_internal_hook.enable();
+		}
+
+		{
+			SIG_INST("40 53 55 56 41 54 41 55 41 56 41 57 48 81 EC 80 00 00 00 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 78 4C 8B 31");
+			auto Curl_ossl_verifyhost = Module(nullptr).range.scan(Pattern(sig_inst)).as<void*>();
+#if LOGGING
+			std::cout << "Curl_ossl_verifyhost = " << Curl_ossl_verifyhost << std::endl;
+#endif
+			Curl_ossl_verifyhost_hook.detour = reinterpret_cast<void*>(&Curl_ossl_verifyhost_detour);
+			Curl_ossl_verifyhost_hook.target = Curl_ossl_verifyhost;
+			Curl_ossl_verifyhost_hook.create();
+			Curl_ossl_verifyhost_hook.enable();
+		}
+
+		// This hook allows WorldSeed to be absent or just any value.
+		{
+			SIG_INST("48 89 5C 24 10 48 89 74 24 18 48 89 7C 24 20 55 41 56 41 57 48 8B EC 48 83 EC 70 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 F0 48 8B D9");
+			auto verify_worldstate_integrity = Module(nullptr).range.scan(Pattern(sig_inst)).as<void*>();
+#if LOGGING
+			std::cout << "verify_worldstate_integrity = " << verify_worldstate_integrity << std::endl;
+#endif
+			verify_worldstate_integrity_hook.detour = reinterpret_cast<void*>(&verify_worldstate_integrity_detour);
+			verify_worldstate_integrity_hook.target = verify_worldstate_integrity;
+			verify_worldstate_integrity_hook.create();
+			verify_worldstate_integrity_hook.enable();
+
+		}
+
+		// This hook allows any WorldSeed be considered valid.
+		/*{
+			SIG_INST("48 89 5C 24 10 48 89 6C 24 18 56 41 54 41 55 41 56 41 57 48 83 EC 40 48 8B AC 24 A8 00 00 00");
+			auto int_rsa_verify = Module(nullptr).range.scan(Pattern(sig_inst)).as<void*>();
+#if LOGGING
+			std::cout << "int_rsa_verify = " << int_rsa_verify << std::endl;
+#endif
+			int_rsa_verify_hook.detour = reinterpret_cast<void*>(&int_rsa_verify_detour);
+			int_rsa_verify_hook.target = int_rsa_verify;
+			int_rsa_verify_hook.create();
+			int_rsa_verify_hook.enable();
+		}*/
+	}
+	return TRUE;
+}
