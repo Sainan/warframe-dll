86e9edf4e55b524c56dbed47c1347d0afb4f2c5a
Sainan <sainan@calamity.gg>
2024-06-25 05:21:07 +0200

    Web page: Init fov override slider with current config value

--- main.cpp
+++ main.cpp
@@ -726,10 +726,12 @@ BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
 							html = ObfusString(R"EOC(<body style="background:#000;color:#fff;">
 	<p>FOV Override (0 = disabled): <input id="fov-override" type="range" min="0" value="0" max="2260000" step="10000"></p>
 	<script>
-		document.getElementById("fov-override").oninput = function()
-		{
+		fetch("http://localhost:61558/fov_override").then(res => res.text()).then(res => {
+			document.getElementById("fov-override").value = parseFloat(res) * 10000;
+		});
+		document.getElementById("fov-override").oninput = function () {
 			fetch("http://localhost:61558/fov_override?" + this.value);
-		}
+		};
 	</script>
 </body>)EOC").str();
 						}
