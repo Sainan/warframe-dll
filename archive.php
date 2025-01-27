490f7d5a539bb1bdb01f0f5f51f5edeb4d763127
Sainan <sainan@calamity.inc>
2025-01-27 11:58:04 +0100

    Pull webui dicts from openwf-translations repo

--- archive.php
+++ archive.php
@@ -6,32 +6,33 @@ function joaat(string $str): int
 	return hexdec(hash("joaat", $str));
 }
 
-function add_file_to_archive($path)
+function add_file_to_archive($path, $archive_path)
 {
 	global $uncompressed;
-	echo $path."\n";
+	echo $path." -> ".$archive_path."\n";
 	$cont = file_get_contents($path);
-	$uncompressed .= pack("V", joaat($path));
+	$uncompressed .= pack("V", joaat($archive_path));
 	$uncompressed .= pack("V", strlen($cont));
 	$uncompressed .= $cont;
 }
 
-function add_folder_to_archive($base)
+function add_folder_to_archive($base, $archive_base)
 {
 	foreach (scandir($base) as $file)
 	{
 		$path = $base.$file;
 		if (is_file($path))
 		{
-			add_file_to_archive($path);
+			$archive_path = $archive_base.$file;
+			add_file_to_archive($path, $archive_path);
 		}
 	}
 
 }
 
-add_folder_to_archive("OpenWF/");
-add_folder_to_archive("OpenWF/samples/");
-add_folder_to_archive("OpenWF/webui_dicts/");
+add_folder_to_archive("OpenWF/", "OpenWF/");
+add_folder_to_archive("OpenWF/samples/", "OpenWF/samples/");
+add_folder_to_archive("../openwf-translations/client-webui/", "OpenWF/webui_dicts/");
 
 $bin_str = gzcompress($uncompressed);
 file_put_contents("owf_archive_data.hpp", "static const char compressed_archive_data[] = { '\\x".join("', '\\x", array_map("dechex", array_map("ord", str_split($bin_str))))."' };");
