6be75bb16ecb3845b74f84fbe637c370ff7b4ba3
Sainan <sainan@calamity.inc>
2025-03-20 21:55:35 +0100

    Bump version

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define BOOTSTRAPPER_TITLE "OpenWF Bootstrapper v0.10.5"
+#define BOOTSTRAPPER_TITLE "OpenWF Bootstrapper v0.10.6"
 
 #define REDIRECT_REQUESTS true
 #define ASK_SERVER_FOR_TUNABLES true
