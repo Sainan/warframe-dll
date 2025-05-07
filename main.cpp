1a791b1cbeebd7584e754a52ab94cabe80ba228f
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-07 07:31:27 +0200

    Bump version

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define BOOTSTRAPPER_TITLE "OpenWF Bootstrapper v0.10.13"
+#define BOOTSTRAPPER_TITLE "OpenWF Bootstrapper v0.10.14"
 
 #define REDIRECT_REQUESTS true
 #define ASK_SERVER_FOR_TUNABLES true
