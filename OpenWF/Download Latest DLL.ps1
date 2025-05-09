98f9d85dee48a6d863dab73c2b00567a18a3c731
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-09 12:06:23 +0200

    Powershell Script: Better handling when using alternative DLL name

--- OpenWF/Download Latest DLL.ps1
+++ OpenWF/Download Latest DLL.ps1
@@ -1,20 +1,21 @@
 Write-Host "Fetching latest version..."
 $latest = Invoke-RestMethod -Uri "https://openwf.io/supplementals/client%20drop-in/meta" -Method Get
 
+$dll_path = "../dwmapi.dll"
+if (Test-Path "../wtsapi32.dll") {
+	$dll_path = "../wtsapi32.dll"
+}
+if (Test-Path "../version.dll") {
+	$dll_path = "../version.dll"
+}
+
 $sha256 = ""
-if (Test-Path "../dwmapi.dll") {
-	$sha256 = (Get-FileHash "../dwmapi.dll" -Algorithm SHA256).Hash.ToLower()
+if (Test-Path $dll_path) {
+	$sha256 = (Get-FileHash $dll_path -Algorithm SHA256).Hash.ToLower()
 }
 $hotfix_sha256 = ""
 if (Test-Path "Hotfix.bin") {
 	$hotfix_sha256 = (Get-FileHash "Hotfix.bin" -Algorithm SHA256).Hash.ToLower()
-}
-
-if (Test-Path "../wtsapi32.dll") {
-	Remove-Item "../wtsapi32.dll"
-}
-if (Test-Path "../version.dll") {
-	Remove-Item "../version.dll"
 }
 
 if ($sha256 -ne $latest.sha256 -or $hotfix_sha256 -ne $latest.hotfix_sha256) {
@@ -26,7 +27,7 @@ if ($sha256 -ne $latest.sha256 -or $hotfix_sha256 -ne $latest.hotfix_sha256) {
 	}
 
 	if ($sha256 -ne $latest.sha256) {
-		Invoke-WebRequest -Uri "https://openwf.io/supplementals/client%20drop-in/$($latest.version)/dwmapi.dll" -OutFile "../dwmapi.dll"
+		Invoke-WebRequest -Uri "https://openwf.io/supplementals/client%20drop-in/$($latest.version)/dwmapi.dll" -OutFile $dll_path
 	}
 	if ($hotfix_sha256 -ne $latest.hotfix_sha256) {
 		if ($latest.hotfix -ne "") {
