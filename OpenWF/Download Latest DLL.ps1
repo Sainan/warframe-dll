141bee8cf8cfb1f08216af8edcb2148855ec48c5
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-22 07:43:25 +0200

    Powershell Script: Sideloadify EXE

--- OpenWF/Download Latest DLL.ps1
+++ OpenWF/Download Latest DLL.ps1
@@ -18,6 +18,16 @@ if (Test-Path "Hotfix.bin") {
 	$hotfix_sha256 = (Get-FileHash "Hotfix.bin" -Algorithm SHA256).Hash.ToLower()
 }
 
+$sideloadify_sha256 = ""
+if (Test-Path "sideloadify-cli.exe") {
+	$sideloadify_sha256 = (Get-FileHash "sideloadify-cli.exe" -Algorithm SHA256).Hash
+}
+if ($sideloadify_sha256 -ne "EE990FF8366E49DE5AF5A80DB845A9BFE9285092365E3A887FDFC76EB20C61AF") {
+	Invoke-WebRequest -Uri "https://github.com/Sainan/Sideloadify/releases/download/1.1.0/sideloadify-cli.exe" -OutFile "sideloadify-cli.exe"
+}
+
+$should_sleep = $false
+
 if ($sha256 -ne $latest.sha256 -or $hotfix_sha256 -ne $latest.hotfix_sha256) {
 	if ($latest.hotfix -ne "") {
 		Write-Host "Downloading OpenWF Bootstrapper v$($latest.version) $($latest.hotfix)..."
@@ -40,5 +50,14 @@ if ($sha256 -ne $latest.sha256 -or $hotfix_sha256 -ne $latest.hotfix_sha256) {
 }
 else {
 	Write-Host "Your OpenWF Bootstrapper is up-to-date."
+	$should_sleep = $true
+}
+
+$sideloadified = (./sideloadify-cli.exe ../Warframe.x64.exe).Contains(" has successfully been sideloadified.")
+if ($sideloadified) {
+	Write-Host "Warframe.x64.exe has successfully been sideloadified."
+	$should_sleep = $true
+}
+if ($should_sleep) {
 	Start-Sleep -Seconds 2
 }
