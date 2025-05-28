f6c4cd8efd1d155addbd4861576e43477a01f399
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-28 20:05:23 +0200

    Expose event types via code

--- owf_scripting.hpp
+++ owf_scripting.hpp
@@ -19,6 +19,7 @@ inline std::string active_input_filter;
 inline bool active_input_filter_allows_hotkeys;
 
 #define OWF_SET_GLOBAL(L, name) { ObfusString os(name); lua_setglobal(L, os.c_str()); }
+#define OWF_EXPOSE_INT_CONSTANT(L, e) lua_pushinteger(L, e); OWF_SET_GLOBAL(L, #e);
 
 enum owfScriptEventType : uint8_t
 {
