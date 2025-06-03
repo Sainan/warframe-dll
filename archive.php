407767115de3d6dea77147e56c8cb01cbf5cbd22
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-03 20:06:43 +0200

    Add pluto to tools so json_minify script always works

--- archive.php
+++ archive.php
@@ -29,7 +29,7 @@ function add_file_to_archive($path, $archive_path)
 	else if (substr($path, -5) == ".json" && $path != "OpenWF/Hotkeys.json")
 	{
 		echo " [minified]";
-		passthru("pluto tools/json_minify.pluto ".escapeshellarg($path)." min.json");
+		passthru("tools\pluto tools/json_minify.pluto ".escapeshellarg($path)." min.json");
 		$cont = file_get_contents("min.json") or die("Failed to minify $path");
 		unlink("min.json");
 	}
