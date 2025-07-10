743228d3065d817171fa5a46ee8f148dba2c54ff
Sainan <63328889+Sainan@users.noreply.github.com>
2025-07-10 18:45:47 +0200

    Move most of the archiving logic to Pluto

--- archive.php
+++ archive.php
@@ -1,55 +1,7 @@
 <?php
-// Do a translation update so all target dicts have all source strings
-{
-	$ogdir = getcwd();
-	chdir("modules/openwf-translations");
-	require "modules/openwf-translations/update.php";
-	chdir($ogdir);
-}
-
-$uncompressed = "";
-
 function joaat(string $str): int
 {
 	return hexdec(hash("joaat", $str));
-}
-
-function add_file_to_archive($path, $archive_path)
-{
-	global $uncompressed;
-	echo $path." -> ".$archive_path;
-	$cont = file_get_contents($path);
-	if ($path == "OpenWF/runtime.pluto")
-	{
-		echo " [compiled]";
-		passthru("tools\plutoc ".escapeshellarg($path));
-		$cont = file_get_contents("plutoc.out") or die("Failed to comile $path");
-		unlink("plutoc.out");
-	}
-	else if (substr($path, -5) == ".json" && $path != "OpenWF/Hotkeys.json")
-	{
-		echo " [minified]";
-		passthru("tools\pluto tools/json_minify.pluto ".escapeshellarg($path)." min.json");
-		$cont = file_get_contents("min.json") or die("Failed to minify $path");
-		unlink("min.json");
-	}
-	echo "\n";
-	$uncompressed .= pack("V", joaat($archive_path));
-	$uncompressed .= pack("V", strlen($cont));
-	$uncompressed .= $cont;
-}
-
-function add_folder_to_archive($base, $archive_base)
-{
-	foreach (scandir($base) as $file)
-	{
-		$path = $base.$file;
-		if (is_file($path))
-		{
-			$archive_path = $archive_base.$file;
-			add_file_to_archive($path, $archive_path);
-		}
-	}
 }
 
 function pack_u64_dyn_v2($v)
@@ -81,11 +33,11 @@ function pack_u64_dyn_v2($v)
 	return $out;
 }
 
-add_folder_to_archive("OpenWF/", "OpenWF/");
-add_folder_to_archive("OpenWF/helpers/", "OpenWF/helpers/");
-add_folder_to_archive("OpenWF/samples/", "OpenWF/samples/");
-add_folder_to_archive("OpenWF/vv/", "OpenWF/vv/");
-add_folder_to_archive("modules/openwf-translations/bootstrapper/", "OpenWF/translations/");
+chdir("tools");
+passthru("pluto archive.pluto");
+chdir("..");
+$uncompressed = file_get_contents("archive.tmp");
+unlink("archive.tmp");
 
 $bin_str = pack_u64_dyn_v2(strlen($uncompressed));
 strlen($bin_str) == 3 or die();
