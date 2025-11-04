2317e0005892035b550d06c6bd935adbf0484933
Sainan <63328889+Sainan@users.noreply.github.com>
2025-11-04 21:22:15 +0100

    > Improve legacy_dns_lookup for U10.6

--- main.cpp
+++ main.cpp
@@ -3637,8 +3637,17 @@ BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
 
 		if (game_version < GV(33, 0, 0))
 		{
-			SIG_INST("40 55 56 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? C6 41 06 01"); // 2016.12.16.14.33
-			const auto legacy_dns_lookup = Module(nullptr).range.scan(sig_inst).as<void*>();
+			void* legacy_dns_lookup;
+			if (game_version >= GV(15, 0, 0))
+			{
+				SIG_INST("40 55 56 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? C6 41 06 01"); // 2016.12.16.14.33
+				legacy_dns_lookup = Module(nullptr).range.scan(sig_inst).as<void*>();
+			}
+			else
+			{
+				SIG_INST("40 55 53 41 54 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 48 83 7A 08 00"); // 2013.11.12.14.03
+				legacy_dns_lookup = Module(nullptr).range.scan(sig_inst).as<void*>();
+			}
 #if LOGGING
 			std::cout << "legacy_dns_lookup = " << legacy_dns_lookup << std::endl;
 #endif
