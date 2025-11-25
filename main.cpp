77f54c44028fa78a1c6b194e5cc9e2e4fdfeb721
Sainan <63328889+Sainan@users.noreply.github.com>
2025-11-25 10:04:54 +0100

    > Add buildVersion to tunables request

--- main.cpp
+++ main.cpp
@@ -757,7 +757,10 @@ struct owfTunablesTask : public soup::Task
 	HttpRequestTask hrt;
 
 	owfTunablesTask()
-		: hrt(HttpRequest(server_host + ":" + std::to_string(game_version >= GV(35, 5, 0) ? https_port : http_port), ObfusString("/custom/tunables.json?clientMod=" BOOTSTRAPPER_TITLE).str()), &Socket::certchain_validator_none)
+		: hrt(HttpRequest(
+			server_host + ":" + std::to_string(game_version >= GV(35, 5, 0) ? https_port : http_port),
+			ObfusString("/custom/tunables.json?clientMod=" BOOTSTRAPPER_TITLE "&buildVersion=").str() + std::string(build_version, 16)
+		), &Socket::certchain_validator_none)
 	{
 		hrt.hr.use_tls = (game_version >= GV(35, 5, 0));
 	}
