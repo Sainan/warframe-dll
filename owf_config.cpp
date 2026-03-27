ec72b53bd12b142791e398874ad5cc792beea9f5
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-27 18:51:31 +0100

    Don't set disable_firewall_prompt by default if not needed SNS will now return an empty NRS array in the login response by default, which doesn't cause a popup afaict.

--- owf_config.cpp
+++ owf_config.cpp
@@ -223,7 +223,7 @@ void owfConfig::load()
 	}
 	else
 	{
-		disable_firewall_prompt = true;
+		disable_firewall_prompt = (game_version <= GV(15, 14, 0));
 	}
 
 	if (auto it = config->reinterpretAsObj().findIt(ObfusString("secure_connections")); it != config->reinterpretAsObj().end() && it->second->isBool())
